781ff4aeaee3a863fe219619fd7c70bb969ba920
rusinha22 <rupalisinha231@gmail.com>
2025-05-21 21:52:44 +0000

    created login page with form - Not functional

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 
    <h1>Assignment 1</h1>
 
+  <p><a href="/login.php">Click here to login</a></p>
+
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>login</title>
+ </head>
+
+ <body>
+
+   <h1>Login form</h1>
+
+   <form action="/validate.php" method="post">
+     <label for="username">Username:</label>
+     <br>
+     <input type="text" id="username" name="fname">
+     <br>
+     <label for="password">Password:</label>
+     <br>
+     <input type="password" id="password" name="Password">
+     <br><br>
+     <input type="submit" value="Submit">
+   </form>
+   
+</body>
+</html>
