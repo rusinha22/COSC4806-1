d0d0ad95297f197d0bb21b70d159cb5e60a6f4f7
rusinha22 <rupalisinha231@gmail.com>
2025-05-31 02:31:23 +0000

    we have created config.php
    in this file we have defined host , user, pass, database and port, to connect to filess.io

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'eyei7.h.filess.io');
+define('DB_USER', 'COSC4806_gulfmetran');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_gulfmetran');
+define('DB_PORT', '3305');
+
+?>
--- index.php
+++ index.php
@@ -1,27 +1,2 @@
 <?php
 
-session_start();
-
-//check if the user is authenticated
-// if not , send them to login.php... header()...
-?>
-
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Page Title</title>
- </head>
-  
- <body>
-
-   <h1>Assignment 1</h1>
-
-   <p> Welcome, <?=$_SESSION['username'] ?></p>
-
-
-</body>
-
-   <footer>
-     <p><a href="/logout.php"> Click here to logout </a></p>
-   </footer>
-</html>
